c06350c54f9c9fdf794208703a7387c4b004c4da
Jou-Valencia, Juan Pablo (UK - London) <jjouvalencia@deloitte.co.uk>
2018-02-22 20:36:36 +0000

    store and redux and folder structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+      applyMiddleware(logger),
+  )
+);
+
+ReactDOM.render( 
+  <Provider store={ store }>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
