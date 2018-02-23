1dcb0f943e1585a3ca5a31dd334a88453e991a5a
Filimon, Anca (UK - London) <alfilimon@deloitte.co.uk>
2018-02-23 11:02:43 +0000

    Initial UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,8 @@ class Header extends Component {
   render() {
     return (
       <header className="App-header">
-        <div className="row">
-        <div className="col-lg-3">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </div>
-        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
       </header>
     )
   }
--- src/components/RoomBox/RoomBox.js
+++ src/components/RoomBox/RoomBox.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react'
+import logo from '../../room.jpg';
 
 class RoomBox extends Component {
   render() {
     return(
       <div className="RoomBox">
-        Helloo
+        <div className="row">
+          <input type="radio" id="roomboxToday" value="today" className="RoomBox-tab" defaultChecked={true} />
+          <label htmlFor="roomboxToday" className="RoomBox-tab-label col-xs-4">TODAY</label>
+          <input type="radio" id="roomboxTomorrow" value="tomorrow" className="RoomBox-tab" />
+          <label htmlFor="roomboxTomorrow" className="RoomBox-tab-label col-xs-4">TOMORROW</label>
+          <input type="radio" id="roomboxPickDate" value="pickDate" className="RoomBox-tab" />
+          <label htmlFor="roomboxPickDate" className="RoomBox-tab-label col-xs-4">PICK DATE</label>
+        </div>
+        <div className="row">
+          <div className="RoomBox-image-container col-xs-12">
+            <img src={logo} className="RoomBox-image" />
+            <p className="RoomBox-image-label">Sheep Room</p>
+          </div>
+
+          <div className="RoomBox-image-container col-xs-12">
+            <img src={logo} className="RoomBox-image" />
+            <p className="RoomBox-image-label">Aylesbury (nee MR1)</p>
+          </div>
+
+          <div className="RoomBox-image-container col-xs-12">
+            <img src={logo} className="RoomBox-image" />
+            <p className="RoomBox-image-label">Hayward's (nee MR2)</p>
+          </div>
+        </div>
       </div>
     )
   }
