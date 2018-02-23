4c163087d4a2a24a0261fe6fe09041501949d996
Radu Asandei <raduasandei@gmail.com>
2018-02-23 14:24:40 +0000

    Added modal.

--- src/components/RoomBox/RoomBox.js
+++ src/components/RoomBox/RoomBox.js
@@ -4,6 +4,7 @@ import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import './../../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
 import events from '../../events';
+import Modal from 'react-modal';
 
 BigCalendar.momentLocalizer(moment);
 
@@ -15,6 +16,32 @@ BigCalendar.momentLocalizer(moment);
   }
 
 class RoomBox extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false,
+      slotInfo: {}
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal(slotInfo) {
+    this.setState({ modalIsOpen: true, slotInfo: slotInfo});
+    console.log(this.state.slotInfo);
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = "#f00";
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
 
   customDayPropGetter = date => {
     return {
@@ -27,57 +54,28 @@ class RoomBox extends Component {
   };
 
   customSlotPropGetter = () => {
-    return { className: "slot-style"};
+    return { className: "slot-style" };
   };
 
+
   render() {
-    return (
-      <div className="RoomBox">
-        <div className="row">
-          <input type="radio" id="roomboxToday" value="today" className="RoomBox-tab" defaultChecked={true} />
-          <label htmlFor="roomboxToday" className="RoomBox-tab-label col-xs-4">TODAY</label>
-          <input type="radio" id="roomboxTomorrow" value="tomorrow" className="RoomBox-tab" />
-          <label htmlFor="roomboxTomorrow" className="RoomBox-tab-label col-xs-4">TOMORROW</label>
-          <input type="radio" id="roomboxPickDate" value="pickDate" className="RoomBox-tab" />
-          <label htmlFor="roomboxPickDate" className="RoomBox-tab-label col-xs-4">PICK DATE</label>
+    return <div className="RoomBox">
+        <div className="RoomBox-image-container col-xs-12, col-md-6 col-lg-4">
+          <img src={this.props.imageSrc} className="RoomBox-image" />
+          <p className="RoomBox-image-label">{this.props.label}</p>
+          <BigCalendar events={events} startAccessor="start" endAccessor="end" selectable={true} defaultView="day" onSelectSlot={(slotInfo) => this.openModal(slotInfo)} views={["day"]} min={new Date(2018, 2, 23, 8, 0, 0)} max={new Date(2018, 2, 23, 18, 0, 0)} defaultDate={new Date()} dayPropGetter={this.customDayPropGetter} eventPropGetter={this.customEventPropGetter} slotPropGetter={this.customSlotPropGetter} components={{ event: Event }} />
         </div>
-        <div className="row">
-          <div className="RoomBox-image-container col-xs-12">
-            <img src={logo} className="RoomBox-image" />
-            <p className="RoomBox-image-label">Sheep Room</p>
-          </div>
-
-          <div className="RoomBox-image-container col-xs-12">
-            <img src={logo} className="RoomBox-image" />
-            <p className="RoomBox-image-label">Aylesbury (nee MR1)</p>
-          </div>
-
-          <div className="RoomBox-image-container col-xs-12">
-            <img src={logo} className="RoomBox-image" />
-            <p className="RoomBox-image-label">Hayward's (nee MR2)</p>
-          </div>
-        </div>
-        <BigCalendar
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          selectable={true}
-          defaultView="day"
-          onSelectSlot={slotInfo => console.log(slotInfo)}
-          views={["day"]}
-          min={new Date(2018, 2, 23, 8, 0, 0)}
-          max={new Date(2018, 2, 23, 18, 0, 0)}
-          defaultDate={new Date()}
-          dayPropGetter={this.customDayPropGetter}
-          eventPropGetter={this.customEventPropGetter}
-          slotPropGetter={this.customSlotPropGetter}
-          components={{
-          event: Event
-          }}
-        />
-      </div>
-    );
+        <Modal isOpen={this.state.modalIsOpen} onAfterOpen={this.afterOpenModal} onRequestClose={this.closeModal} contentLabel="Example Modal">
+          <h2 ref={subtitle => (this.subtitle = subtitle)}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>{this.state.slotInfo.action}</div>
+          <form>
+            <input />
+          </form>
+        </Modal>
+      </div>;
   }
 }
+
 
 export default RoomBox;
